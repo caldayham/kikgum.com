2c91f77642dd2f9a6ff0751d867367cfddf1ec74
caldayham <caldayham@gmail.com>
2024-02-05 00:22:13 -0600

    amplify.yml back

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "KIK Gum",
+  title: "Enter flow.",
   description: "Clean energy for a busy schedule.",
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,16 @@ import BuyButton from "@/components/BuyButton";
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main_wrapper}>
       <div className={styles.headline_wrapper}>
-        <h1>
-          KIK Gum
-        </h1>
-        <p>All natural caffeinated gum with L-theanine and B vitamins.</p>
-        <p>Clean energy and focus on a busy schedule.</p>
+        <Image src="/kik-logo-v2.png" width={400} height={400} alt="kik logo"/>
+
+        <h1>Your tool for <span style={{background: 'black', color: 'white', padding: '0.5rem 1rem', fontStyle: 'italic'}}>FLOW</span></h1>
 
         <BuyButton>Order Now!</BuyButton>
+
+
+        <p>All natural caffeinated gum with L-theanine and B vitamins. Clean energy and flow on a busy schedule.</p>
       </div>
     </main>
   );
